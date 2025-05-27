98fa4b80f1bc1fb32bfc6d31720f6cf244a5a0af
Hassan <hassanmurtaza.moshlay@gmail.com>
2025-05-28 03:27:25 +0500

    updated code

--- index.js
+++ index.js
@@ -16,23 +16,23 @@ dotenv.config();
 // 2. Server Setup
 // ============================================
 let server;
-if (process.env.NODE_ENV === "PRODUCTION") {
-  try {
-    const privateKey = fs.readFileSync("./privkey.pem", "utf8");
-    const certificate = fs.readFileSync("./fullchain.pem", "utf8");
+// if (process.env.NODE_ENV === "PRODUCTION") {
+//   try {
+//     const privateKey = fs.readFileSync("./privkey.pem", "utf8");
+//     const certificate = fs.readFileSync("./fullchain.pem", "utf8");
 
-    const options = {
-      key: privateKey,
-      cert: certificate,
-    };
+//     const options = {
+//       key: privateKey,
+//       cert: certificate,
+//     };
 
-    server = https.createServer(options, app);
-  } catch (err) {
-    console.error("Error while reading pem files:", err);
-  }
-} else {
-  server = http.createServer(app);
-}
+//     server = https.createServer(options, app);
+//   } catch (err) {
+//     console.error("Error while reading pem files:", err);
+//   }
+// } else {
+server = http.createServer(app);
+// }
 
 // ============================================
 // 3. Server Listening & DB Connection
@@ -41,7 +41,7 @@ const PORT = process.env.PORT || 3000;
 (async () => {
   try {
     await connectDb();
-    server.listen(PORT, () => {
+    server.listen(PORT, "0.0.0.0", () => {
       if (process.env.NODE_ENV === "PRODUCTION") {
         console.log(`Server is running on port ${PORT}`);
       } else {
